a4fe9c2d778c9c17a1967056cb5007016482a348
Oscar <strange2155@yahoo.com>
2018-06-26 01:39:06 -0500

    working on gallery desktop version

--- js/gallery.js
+++ js/gallery.js
@@ -1,19 +1,27 @@
 
 let car1 = document.querySelector(".car1");
 let car2 = document.querySelector(".car2");
+let scar1 = document.querySelector(".s-car1");
+let scar2 = document.querySelector(".s-car2");
 let image1 = document.querySelector(".image1");
 let image2 = document.querySelector(".image2");
 
 
 
     image2.addEventListener("click", function(){
-       car1.style.display= "none";
-       car2.style.display= "";
+        scar1.style.display= "none";
+        scar2.style.display= "";
+        
+        car1.style.display= "none";
+        car2.style.display= "";
        
 
     });
 
-    image1.addEventListener("click", function(){
+    image1.addEventListener("click", function(){ 
+        scar2.style.display= "none";
+        scar1.style.display= "";
+       
         car2.style.display= "none";
         car1.style.display= "";
 
