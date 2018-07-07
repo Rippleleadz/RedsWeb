bd06850483138823935fdecd008c2d987e6abe1b
Oscar <strange2155@yahoo.com>
2018-07-07 17:37:30 -0500

    contact page set up

--- js/video.js
+++ js/video.js
@@ -1,33 +1,59 @@
-let svid1 = document.querySelector(".s-vid1");
-let svid2 = document.querySelector(".s-vid2");
-let svid3 = document.querySelector(".s-vid3");
+
+
+let vid1 = document.querySelector(".vid1");
+let vid2 = document.querySelector(".vid2");
+let vid3 = document.querySelector(".vid3");
 let image1 = document.querySelector(".image1");
 let image2 = document.querySelector(".image2");
 let image3 = document.querySelector(".image3");
 
 
+var iframe = document.getElementsByTagName("iframe"); 
+
+function click() { 
+
+    if(image3){
+    iframe.pause(); 
+    }
+} 
 
     image1.addEventListener("click", function(){
-        svid1.style.display= "block";
-        svid2.style.display= "none";
-        svid3.style.display= "none";
+        vid1.style.display= "block";
+        vid2.style.display= "none";
+        vid3.style.display= "none";
        
 
     });
 
     image2.addEventListener("click", function(){ 
-        svid2.style.display= "block";
-        svid1.style.display= "none";
-        svid3.style.display= "none";
+        vid2.style.display= "block";
+        vid1.style.display= "none";
+        vid3.style.display= "none";
         
  
      });
 
      image3.addEventListener("click", function(){ 
-        svid3.style.display= "block";
-        svid1.style.display= "none";
-        svid2.style.display= "none";
+        vid3.style.display= "block";
+        vid1.style.display= "none";
+        vid2.style.display= "none";
        
         
  
      });
+
+//      /**
+//  * Stop an iframe or HTML5 <video> from playing
+//  * @param  {Element} element The element that contains the video
+//  */
+// var stopVideo = function ( element ) {
+// 	var iframe = element.querySelector( 'iframe');
+// 	var video = element.querySelector( 'video' );
+// 	if ( iframe !== iframe ) {
+// 		var iframeSrc = iframe.src;
+// 		iframe.src = iframeSrc;
+// 	}
+// 	if ( iframeSrc !== ) {
+// 		video.pause();
+// 	}
+// };
