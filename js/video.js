db8719424a7d660a4058427d538af9faaedf3473
Oscar <strange2155@yahoo.com>
2018-07-09 15:30:48 -0500

    re-did video gallery with working thumbnails

--- js/video.js
+++ js/video.js
@@ -1,59 +1,18 @@
-
-
-let vid1 = document.querySelector(".vid1");
-let vid2 = document.querySelector(".vid2");
-let vid3 = document.querySelector(".vid3");
-let image1 = document.querySelector(".image1");
-let image2 = document.querySelector(".image2");
-let image3 = document.querySelector(".image3");
-
-
-var iframe = document.getElementsByTagName("iframe"); 
-
-function click() { 
-
-    if(image3){
-    iframe.pause(); 
-    }
-} 
-
-    image1.addEventListener("click", function(){
-        vid1.style.display= "block";
-        vid2.style.display= "none";
-        vid3.style.display= "none";
-       
-
+$(document).ready(function() {
+    $('#section-b a').each(function() {
+      var data = $(this).attr('data');
+      $(this).append('<img src="http://img.youtube.com/vi/'+data+'/maxresdefault.jpg" />');
     });
-
-    image2.addEventListener("click", function(){ 
-        vid2.style.display= "block";
-        vid1.style.display= "none";
-        vid3.style.display= "none";
-        
- 
-     });
-
-     image3.addEventListener("click", function(){ 
-        vid3.style.display= "block";
-        vid1.style.display= "none";
-        vid2.style.display= "none";
-       
-        
- 
-     });
-
-//      /**
-//  * Stop an iframe or HTML5 <video> from playing
-//  * @param  {Element} element The element that contains the video
-//  */
-// var stopVideo = function ( element ) {
-// 	var iframe = element.querySelector( 'iframe');
-// 	var video = element.querySelector( 'video' );
-// 	if ( iframe !== iframe ) {
-// 		var iframeSrc = iframe.src;
-// 		iframe.src = iframeSrc;
-// 	}
-// 	if ( iframeSrc !== ) {
-// 		video.pause();
-// 	}
-// };
+  
+    $('#section-b a').click(function() {
+      var data = $(this).attr('data');
+      $('#loader').append('<iframe src="https://www.youtube.com/embed/'+data+'?autoplay=1" frameborder="0" allowfullscreen rel=0 ></iframe>');
+      $('#overlay').fadeIn(250);
+    });
+  
+    $('#close').click(function() {
+      $('#overlay').fadeOut(250,function() {
+        $('#loader').html('');
+      });
+    });
+  });
